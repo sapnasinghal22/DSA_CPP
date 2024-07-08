a3ffa6dc77abf4a94b87f94f9b9531ecfb3788b8
Sapna <144775060+sapnasinghal22@users.noreply.github.com>
2024-07-08 15:38:12 +0530

    Create min stack.cpp

---
+++ stack/min stack.cpp
@@ -0,0 +1,53 @@
+class MinStack {
+public:
+vector <pair <int,int> > st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if (st.empty())
+        {
+            pair<int,int> p ;
+            p.first=val;
+            p.second=val;
+            st.push_back(p);
+        }
+        else
+        {
+            pair<int,int> p ;
+            p.first= val;
+            int puranamin = st.back().second;
+            p.second= min(puranamin,val);
+            st.push_back(p);
+
+        }
+        
+    }
+    
+    void pop() {
+        st.pop_back();
+        
+    }
+    
+    int top() {
+        pair<int,int> rightmost= st.back();
+        return rightmost.first;
+        
+    }
+    
+    int getMin() {
+        pair<int,int> rightmost = st.back();
+        return rightmost.second;
+        
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
