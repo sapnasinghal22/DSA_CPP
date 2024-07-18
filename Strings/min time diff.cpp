9c1ddc24c8d97fb21a1a4d265036fee04fb8ed78
Sapna <144775060+sapnasinghal22@users.noreply.github.com>
2024-07-19 01:56:31 +0530

    Create min time diff.cpp

---
+++ Strings/min time diff.cpp
@@ -0,0 +1,26 @@
+// leetcode:-539
+class Solution {
+public:
+int convertToMin(string &time)
+{
+    int hh = stoi(time.substr(0,2));
+    int mm = stoi(time.substr(3,2));
+    return 60*hh+mm;
+}
+    int findMinDifference(vector<string>& timePoints) {
+     vector<int> mins;
+     for(auto time:timePoints)
+     {
+        mins.push_back(convertToMin(time));
+     }   
+     sort(mins.begin(),mins.end());
+     int ans = INT_MAX;
+     for(int i=0;i<mins.size()-1;i++)
+     {
+        ans = min(ans ,mins[i+1]-mins[i]);
+        int lastdiff = mins[0]+1440- mins[mins.size()-1];
+        ans = min(ans ,lastdiff);
+     }
+     return ans ;
+    }
+};
