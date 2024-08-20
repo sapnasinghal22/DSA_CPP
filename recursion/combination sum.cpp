ce43fa84afa6f65ffeb3371ff231b16a32b562b8
Sapna <144775060+sapnasinghal22@users.noreply.github.com>
2024-08-21 02:35:06 +0530

    Create combination sum.cpp

---
+++ recursion/combination sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+void solve (vector<int> & candidates,int target,vector<int> & v,vector<vector<int>> & ans ,int index )
+{
+    if(target==0)
+    {
+        ans.push_back(v);
+        return ;
+    }
+    if(target<0)
+    {
+        return ;
+    }
+    for(int i=index;i<candidates.size();i++)
+    {
+        v.push_back(candidates[i]);
+        solve(candidates,target-candidates[i],v,ans,i);
+        v.pop_back();
+
+    }
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> v ;
+        vector<vector<int>> ans ;
+        solve (candidates,target,v,ans,0);
+        return ans ;
+        
+    }
+};
